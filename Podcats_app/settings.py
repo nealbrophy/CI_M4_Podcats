b7408a6f3fa28dfb9aa28889ce83fd76df645cee
Neal <nbrophy@pm.me>
2020-07-12 15:12:25 +0100

    Add home app and tempaltes

--- Podcats_app/settings.py
+++ Podcats_app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,10 @@ ROOT_URLCONF = 'Podcats_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
