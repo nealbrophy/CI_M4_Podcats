0813b26373f86f52d488b2c66c70c3d4a6f656a6
Neal Brophy <nbrophy@pm.me>
2020-07-29 22:26:11 +0100

    Add upgrade_success view, continue working on upgrade purchase.

--- pro/static/pro/js/stripe_elements.js
+++ pro/static/pro/js/stripe_elements.js
@@ -1,23 +1,69 @@
-const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-const stripe_client_secret = $('#id_client_secret').text().slice(1, -1);
+const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+const stripeClientSecret = $('#id_client_secret').text().slice(1, -1);
 const stripe = Stripe(stripe_public_key);
 const elements = stripe.elements();
 
 
 var style = {
-  base: {
-    color: '#32325d',
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: 'antialiased',
-    fontSize: '16px',
-    '::placeholder': {
-      color: '#aab7c4'
+    base: {
+        color: '#32325d',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
     }
-  },
-  invalid: {
-    color: '#dc3545',
-    iconColor: '#dc3545'
-  }
 };
-const card = elements.create('card', {style: style});
-card.mount('#card-element');
+const card = elements.create("card", {style: style});
+card.mount("#card-element");
+
+card.addEventListener("change", function (event) {
+    let errorDiv = document.getElementById("card-errors");
+    if (event.error) {
+        let html = `
+            <span class="icon text-danger" role="alert">
+            <i class="fas fa-times"></i>
+            </span>
+            <span class="text-danger">${event.error.message}</span>`;
+
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Submit payment
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    card.update({'disabled': true});
+    $('#submit-button').attr('disabled', true)
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function (result) {
+        if (result.error) {
+            let errorDiv = document.getElementById("card-errors");
+            let html = `
+                <span class="icon text-danger" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span class="text-danger">${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({'disabled': false});
+            $('#submit-button').attr('disabled', false)
+        } else {
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- pro/views.py
+++ pro/views.py
@@ -1,32 +1,72 @@
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .models import Purchase
 from .forms import PurchaseForm
+
+import stripe
 
 def upgrade(request):
     """ A review to return the pro page. """
     user = User.objects.get(id=request.user.id)
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if user.userprofile.pro_user:
-        messages.info(request, "You already have a Pro account! You rock!")
-        profile = user.userprofile
-        context = {
-            "profile": profile,
-        }
-        return redirect('dashboard')
+    if request.method == "GET":
+        if user.userprofile.pro_user:
+            messages.info(request, "You already have a Pro account! You rock!")
+            profile = user.userprofile
+            context = {
+                "profile": profile,
+            }
+            return redirect('dashboard')
+        else:
+            form = PurchaseForm()
+            cost = settings.PRO_ANNUAL_CHARGE
+            stripe_cost = round(cost * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_cost,
+                currency=settings.STRIPE_CURRENCY,
+            )
+
+            if not stripe_public_key:
+                messages.warning(request, "Stripe public key is missing.")
+
+            context = {
+                "stripe_public_key": stripe_public_key,
+                "client_secret": intent.client_secret,
+                "form": form,
+                "user": user,
+                "cost": cost,
+            }
+            return render(request, "pro/upgrade.html", context)
     else:
-        form = PurchaseForm()
+        form_data = {
+            "full_name": request.POST["full_name"],
+            "email": request.POST["email"],
+            "phone_number": request.POST["phone_number"],
+            "postcode": request.POST["postcode"],
+            "town_or_city": request.POST["town_or_city"],
+            "street_address1": request.POST["street_address1"],
+            "street_address2": request.POST["street_address2"],
+            "country": request.POST["country"],
+        }
 
-    context = {
-        "stripe_public_key": "pk_test_51GwAMXHTAoCBcf2ymYxl3YLq4QePvZxEQWcPzyt6Ei3ZyYSa0XXRfm3W16T7q5ZH3SR81ptvMuy4G0Pyt4LfF8Dc00PcEOy9D9",
-        "client_secret": "test client secret",
-        "form": form,
-        "user": user,
-    }
+        purchase_form = PurchaseForm(form_data)
+        if purchase_form.is_valid():
+            purchase = purchase_form.save(commit=False)
+            pid = request.POST.get("client_secret").split("_secret")[0]
+            purchase.stripe_pid = pid
+            purchase.save()
 
-    return render(request, "pro/upgrade.html", context)
+        return redirect(reverse("upgrade_success", args=[purchase.order_number]))
 
+def upgrade_success(request, order_number):
+    """ A view to handle successful upgrade purchase """
+    if request.user.is_authenticated:
+        pass
 
 def benefits(request):
     """ A review to return the benefits page. """
